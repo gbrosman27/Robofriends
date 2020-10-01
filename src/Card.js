32b3a07b94b3956cb8c03f18b109930406c323f8
Greg Brosman <greg.brosman@gmail.com>
2020-10-01 15:19:45 -0700

    formatting

--- src/Card.js
+++ src/Card.js
@@ -7,7 +7,7 @@ const Card = (props) => {
         // text center, background color, border radius, padding, margin, grow on hover, shadow.
         // Generate random robot image for each card from robohash api.
         <div className='tc bg-light-green dib br3 pa2 ma2 grow bw2 shadow-5'>
-            <img alt='chelsea_photo' src={`https://robohash.org/${props.id}?200x200`}/> 
+            <img alt='robot_photo' src={`https://robohash.org/${props.id}?200x200`}/> 
             <div>
                 <h2>{props.name}</h2>
                 <p>{props.email}</p>

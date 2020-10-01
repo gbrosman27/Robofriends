172033ea9859dca267c73ce478a27ef12a97d9a1
Greg Brosman <greg.brosman@gmail.com>
2020-10-01 14:34:55 -0700

    CardList.js map function

--- src/CardList.js
+++ src/CardList.js
@@ -3,14 +3,21 @@ import Card from './Card';
 
 // Use map to loop through all contact entries and create a card for each of them.
 const CardList = ({robots}) => {
-    const cardComponent = robots.map((user, i) => {
-        return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
-    })
-    return(
-            <div>
-                {cardComponent}
-            </div>
-    );
-}
+        return ( 
+            <div> 
+                {
+                robots.map((user, i) => {
+                    return (
+                            <Card 
+                            key = {i}
+                            id = {robots[i].id}
+                            name = {robots[i].name}
+                            email = {robots[i].email}
+                            />
+                            )
+                    })
+                } 
+            </div>)
+        }
 
 export default CardList;

340a6d315a96622a9c31767ea3044e93e2694932
Greg Brosman <greg.brosman@gmail.com>
2020-10-02 14:52:22 -0700

    searchbox functionality

--- src/App.js
+++ src/App.js
@@ -4,15 +4,32 @@ import SearchBox from './SearchBox';
 // Destructured format because robots.js is export, not export default.
 import { robots } from './robots';
 
-const App = () => {
-    return(
-        // Text centered.
-        <div className='tc'> 
-            <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots={robots}/>
-        </div>
-    );
+
+class App extends React.Component {
+    constructor(){
+        super()
+        // state can change and affect the app.
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    //Everytime input changes, we get an event. Then console log the event.
+    onSearchChange(event){
+        console.log(event.target.value);
+    }
+
+    render(){    
+        return(
+            // Text centered.
+            <div className='tc'> 
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={this.state.robots}/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-const SearchBox = () => {
+
+//Use destructuring to grab the props object and it's properties.
+const SearchBox = ({searchChange}) => {
     return(
         <div className='pa2'>
         <input 
             className='pa3 ba b--green bg-light-blue' 
             type='search' 
-            placeholder='search contacts'/>
+            placeholder='search contacts'
+            onChange={searchChange}
+        />
         </div>
     );
 }

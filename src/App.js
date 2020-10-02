daf3bb2d119527053052c1cbbe95b3d1b387b761
Greg Brosman <greg.brosman@gmail.com>
2020-10-02 14:27:29 -0700

    centered text in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import CardList from './CardList';
+import SearchBox from './SearchBox';
 // Destructured format because robots.js is export, not export default.
 import { robots } from './robots';
 
 const App = () => {
     return(
-    <CardList robots={robots}/>
+        // Text centered.
+        <div className='tc'> 
+            <h1>RoboFriends</h1>
+            <SearchBox />
+            <CardList robots={robots}/>
+        </div>
     );
 }
 
-export default App
+export default App;

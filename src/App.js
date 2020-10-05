0da64744843d8b22676167dea5b77c0f2787a577
Greg Brosman <greg.brosman@gmail.com>
2020-10-05 14:06:12 -0700

    add componentDidMount function

--- src/App.js
+++ src/App.js
@@ -2,25 +2,36 @@ import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import './App.css';
+
+
 // Destructured format because robots.js is export, not export default.
-import { robots } from './robots';
+//import { robots } from './robots';
 
 
+// Smart Component due to having 'state' (data the describes the app).
 class App extends React.Component {
     constructor(){
         super()
-        // state can change and affect the app.
+        // state can change and affect the app. Run with empty array.
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
 
-    //Filter robots according to searchfield.
+    // Fetch from url, get users and update users with setState.
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}));
+    }
+
+    //Filter robots according to searchfield. Use arrow functions since this is not built into react.
     onSearchChange = (event) => {
         this.setState({searchfield: event.target.value})
     }
 
+    // Render the app dsiplay.
     render(){    
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());

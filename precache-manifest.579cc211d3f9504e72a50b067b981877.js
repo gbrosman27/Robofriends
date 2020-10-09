bb1053104e0fe1be0ec5e61a4d6ec7c019023a15
Greg Brosman <greg.brosman@gmail.com>
2020-10-09 14:02:04 -0700

    Updates

---
+++ precache-manifest.579cc211d3f9504e72a50b067b981877.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81bf463af9a3b0197ea3c6f71d6fd7e7",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/Robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "297dcb9ef97309f7cd84",
+    "url": "/Robofriends/static/css/main.acafdd23.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/Robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "297dcb9ef97309f7cd84",
+    "url": "/Robofriends/static/js/main.0f4f278f.chunk.js"
+  },
+  {
+    "revision": "a6103a62c81f6271b765",
+    "url": "/Robofriends/static/js/runtime-main.79cc7977.js"
+  }
+]);
